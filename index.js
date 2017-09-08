5dc8cce46ecd7d6261b7d9a7e2d733961b43bd08
Nate <nhargitt@bayareanewsgroup.com>
2017-09-07 22:38:17 -0700

    remove answer from problem

--- index.js
+++ index.js
@@ -49,35 +49,3 @@ var data = [
         "price": "$14.99"
     },
 ]
-
-//container for all menu items, may be a good place to append some columns
-var menuContainer = $('#menuContainer');
-
-for( var i = 0; i < data.length; i++) {
-    //bootstrap column needed
-    var column = document.createElement('div');
-        $(column).addClass('col-md-3');
-
-    var title = document.createElement('h4');
-        title.innerHTML = data[i].name;
-        $(title).addClass('menu-item-name')
-        column.append(title);
-
-    var container = document.createElement('div');
-        $(container).addClass('description-container')
-
-    var description = document.createElement('p');
-        description.innerHTML = data[i].description;
-        $(description).addClass('description');
-        container.append(description);
-
-    var price = document.createElement('p');
-        price.innerHTML = data[i].price;
-        $(price).addClass('price');
-        container.append(price);
-
-        column.append(container);
-
-        menuContainer.append(column);
-
-}
